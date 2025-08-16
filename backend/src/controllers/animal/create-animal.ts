2388a8b459040ba0e1d38ae85399135a07b25ddb
unknown <marciopaivadev@gmail.com>
2025-08-16 16:29:33 -0300

    pr3-code smell 1

--- backend/src/controllers/animal/create-animal.ts
+++ backend/src/controllers/animal/create-animal.ts
@@ -1,41 +1,37 @@
 import { Request, Response } from 'express'
+import { BaseController } from '../base-controller'
 import {
   CreateAnimalService,
   createAnimalServiceInstance,
-} from '../../services/animal/create-animal.js'
+} from '../../services/animal/create-animal'
 
-class CreateAnimalController {
-  constructor(private readonly createAnimal: CreateAnimalService) {}
+class CreateAnimalController extends BaseController {
+  constructor(private readonly createAnimal: CreateAnimalService) {
+    super()
+  }
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, type, gender, race, description } = request.body
     const { user } = request
     const pictures = request.files as Express.Multer.File[]
+    const pictureBuffers = pictures.map((file) => file.buffer)
 
-    try {
-      const pictureBuffers = pictures.map((file) => file.buffer)
-
-      const result = await this.createAnimal.execute({
-        name,
-        type,
-        gender,
-        race,
-        description,
-        userId: user?.id || '',
-        pictures: pictureBuffers,
-      })
-
-      const statusCode = result.isFailure() ? 400 : 201
-
-      return response.status(statusCode).json(result.value)
-    } catch (err) {
-      const error = err as Error
-      console.error('Error creating animal:', error)
-      return response.status(500).json({ error: error.message })
-    }
+    return this.execute(
+      () =>
+        this.createAnimal.execute({
+          name,
+          type,
+          gender,
+          race,
+          description,
+          userId: user?.id || '',
+          pictures: pictureBuffers,
+        }),
+      response
+    )
   }
 }
 
 export const createAnimalControllerInstance = new CreateAnimalController(
-  createAnimalServiceInstance,
+  createAnimalServiceInstance
 )
--- backend/src/controllers/user/create-user.ts
+++ backend/src/controllers/user/create-user.ts
@@ -1,29 +1,24 @@
 import { Request, Response } from 'express'
+import { BaseController } from '../base-controller'
 import {
   CreateUserService,
   createUserServiceInstance,
-} from '../../services/user/create-user.js'
+} from '../../services/user/create-user'
 
-class CreateUserController {
-  constructor(private readonly createUser: CreateUserService) {}
+class CreateUserController extends BaseController {
+  constructor(private readonly createUser: CreateUserService) {
+    super()
+  }
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, password } = request.body
-
-    try {
-      const result = await this.createUser.execute({ name, email, password })
-
-      const statusCode = result.isFailure() ? 400 : 201
-
-      return response.status(statusCode).json(result.value)
-    } catch (err) {
-      const error = err as Error
-      console.log({ error })
-      return response.status(500).json({ error: error.message })
-    }
+    return this.execute(
+      () => this.createUser.execute({ name, email, password }),
+      response
+    )
   }
 }
 
 export const createUserControllerInstance = new CreateUserController(
-  createUserServiceInstance,
+  createUserServiceInstance
 )
